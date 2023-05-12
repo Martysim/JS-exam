63351579b1dff011ba98ad4a8af3d4781ed4a9d7
Martysim <martin.simeonov@scalefocus.com>
2023-05-12 19:07:31 +0300

    fun-arrOfObj-return-prop-name

--- fun_ArrOfObjPropName_return_newArr.js
+++ fun_ArrOfObjPropName_return_newArr.js
@@ -1,20 +1,48 @@
 // Write a function that takes in an array of objects and a property name, and returns a new array with the values of the specified property from each object.
+// let arrObj = [
+//     {
+//       prop: 1,
+//       prop2: 2
+//     },
+//     {
+//       prop: 4,
+//       prop3: 5
+//     },
+//     {
+//       prop: 7,
+//       prop5: 8 
+//     }
+//   ];
+//   function idk (arrObj, propName) {
+//   };
+//   result = [1, 4, 7];
 
-let arrObj = [
-    {
-      prop: 1,
-      prop2: 2
-    },
-    {
-      prop: 4,
-      prop3: 5
-    },
-    {
-      prop: 7,
-      prop5: 8 
-    }
-  ];
-  function idk (arrObj, propName) {
-  
+let arrOfObj = [
+  {
+    prop: 1,
+    prop2: 4
+  },
+  {
+    prop: 3,
+    prop3: 5
+  },
+  {
+    prop: 5,
+    prop4: 7
+  },
+  {
+    prop: 6,
+    prop5: 8
+  }
+
+];
+
+function arrOfObjInArrOfProp (arrObj, key) {
+  let result = [];
+  for (var i = 0; i < arrObj.length; i++) {
+    result.push(arrObj[i][key]);
   };
-  result = [1, 4, 7];
+  return result;
+};
+
+console.log(arrOfObjInArrOfProp(arrOfObj, "prop"));
