f1a2ccd6226e475813cc9214c77adfa1856c60e6
Martysim <martin.simeonov@scalefocus.com>
2023-05-28 18:42:22 +0300

    add-exersice-with-objArr

---
+++ create_funTakeArrOfObj_returnArrOfProp.js
@@ -0,0 +1,30 @@
+// Create a function that takes an array of objects and returns an array of the values of a specified property.
+
+console.log("------------------------------------");
+
+const arrOfObj3 = [
+    {
+        age: 18,
+        name: "martin"
+    },
+    {
+        age: 19,
+        name: "alex"
+    }
+];
+
+
+
+
+function getPropertyValues(arr, property) {
+    return arr.map(obj => obj[property]);
+  
+};
+  
+console.log(getPropertyValues(arrOfObj3, "age"));
+console.log(getPropertyValues(arrOfObj3, "name"));
+
+
+function getOwnProperty (arr) {
+    
+}
---
+++ fun_takeObj_returnString.js
@@ -0,0 +1,37 @@
+// Write a function that takes an object and returns a string in the format "property: value".
+
+console.log("----------------------------------------------------");
+
+const objS = {
+    property: "value",
+    property2: "value2",
+    property3: "value3",
+
+};
+
+function objInString (obj) {
+    let result = "";
+    for (let key in obj) {
+        result += ` ${key}:${obj[key]}  `;
+    };
+    return result;
+};
+
+console.log(objInString(objS));
+
+
+
+
+
+// function formatObject(obj) {
+//     let result = "";
+//     for (let key in obj) {
+//       if (obj.hasOwnProperty(key)) {
+//         result += `${key}: ${obj[key]}\n`;
+//       }
+//     }
+//     return result;
+//   }
+  
+  
+  
